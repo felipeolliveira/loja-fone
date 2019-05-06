8ec6f6167e2546b75f34da9972457944cc562d6e
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-06 15:25:12 +0000

    Aula 14 - Desafio intermediário

--- js/product-description.js
+++ js/product-description.js
@@ -1,8 +1,16 @@
 const $favoriteIcon = document.querySelector(".-favorite");
+const $starsIcons = document.querySelectorAll(".star");
+
+console.log($starsIcons);
 
 const handleClickFavoriteIcon = () => {
   $favoriteIcon.classList.toggle("-active");
-  console.log("teste");
 };
 
 $favoriteIcon.addEventListener("click", handleClickFavoriteIcon);
+
+$starsIcons.forEach(star => {
+  star.addEventListener("click", () => {
+    star.classList.toggle("-active");
+  });
+});
