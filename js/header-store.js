c04bf3b185d19a65fb80ce7004ff75c851996502
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-09 20:47:08 +0000

    Aula 017 - Todos os desafios

--- js/header-store.js
+++ js/header-store.js
@@ -1,7 +1,8 @@
-const $search = document.querySelector('[type="search"]');
+const $icons = document.querySelectorAll(".header-store .icon");
+const $searchBar = document.querySelector(".header-store .search");
 
 function handleClick() {
-  this.classList.toggle("-active");
+  $searchBar.classList.toggle("-active");
 }
 
-$search.addEventListener("click", handleClick);
+$icons.forEach(icon => icon.addEventListener("click", handleClick));
