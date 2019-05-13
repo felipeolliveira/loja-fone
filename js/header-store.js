28e14fd861aa1a65bfdbd2217bae1765afe8fd17
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-13 17:27:16 +0000

    Adicionado o reset no campo de busca quando for iniciado. Para não abrir com o último texto digitado

--- js/header-store.js
+++ js/header-store.js
@@ -1,9 +1,11 @@
-const $icons = document.querySelectorAll(".header-store .icon");
+const $icons = document.querySelectorAll(".search-bar .icon");
+const $content = document.querySelector(".search-bar .content");
 const $searchBar = document.querySelector(".search-bar");
 
 function handleClick(event) {
   event.preventDefault();
   $searchBar.classList.toggle("-active");
+  if ($content.value) $content.value = "";
 }
 
 $icons.forEach(icon =>
